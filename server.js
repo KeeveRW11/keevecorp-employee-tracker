6c9fedfe419f4ae6e91976e47b4ad1e9fcd128b3
KeeveRW11 <keevewhyte+github@gmail.com>
2022-09-21 13:47:00 -0400

    create sql tables

---
+++ server.js
@@ -0,0 +1,25 @@
+const db = require('./db/connection');
+const express = require('express');
+//const inputCheck = require('./utils/inputCheck');
+const apiRoutes = require('./routes/apiRoutes');
+
+const PORT = process.env.PORT || 3006;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+// Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
