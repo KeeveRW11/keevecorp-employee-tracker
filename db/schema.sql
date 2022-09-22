3e6b24a1a2cdda0ec46fbc4145f6baaf0655e804
KeeveRW11 <keevewhyte+github@gmail.com>
2022-09-22 11:45:20 -0400

    seed roles

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,24 @@
+DROP DATABASE IF EXISTS employee;
+CREATE DATABASE employee;
+USE employee;
+
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    name VARCHAR(30);
+    name VARCHAR(30) NOT NULL
 );
 
 
-CREATE TABLE role_id (
-    id INTEGER  PRIMARY KEY,
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INTEGER
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+
 );
 
 CREATE TABLE employee (
-    id INTEGER  PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER,
--- db/seed.sql
+++ db/seed.sql
@@ -4,3 +4,11 @@ VALUES
 ('Engineering'),
 ('Finance'),
 ('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Business Development Lead', 90000, 1),
+('Full Stack Developer', 80000, 2),
+('Accountant', 10000, 3), 
+('Chief Finanical Officer', 150000, 3),
+('Chief Compliance Officer', 90000, 4);
